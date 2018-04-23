c40753968079ddac4cfb76b52cc2bbfd55d06d73
RegulusImpact <rfinley93@gmail.com>
2018-04-23 09:58:08 -0600

    cart header changes

--- src/Cartridge.h
+++ src/Cartridge.h
@@ -15,15 +15,38 @@
 
 class Cartridge {
 private:
+    std::string filePath;
     uint8_t* rom;
+    uint8_t* ram;
     bool fileLoaded;
     uint32_t fileSize;
+    uint32_t maxRamSize;
+
+
 
 public:
+    // Cart data
+    char title[16]; // 0x134 - 0x143
+    bool sgbFlag; // 0x146
+    uint16_t cartType; // 0x147
+    uint16_t romSize; // 0x148
+    uint16_t ramSize; // 0x149
+    bool destinationCode; // 0x14A - true = nonJP
+
     Cartridge(std::string fileName);
     ~Cartridge();
 
+    void LoadFile();
+    void LoadHeader();
+    void CreateRAM();
+    bool HeaderChecksum();
+
     uint8_t Read(uint32_t addr);
+    uint8_t ReadRAM(uint32_t addr);
+    void WriteRAM(uint32_t addr, uint8_t val);
+
+    void DumpInfo(); // pretty print
+
     uint32_t GetFileSize() { return fileSize; }
     bool IsLoaded() { return fileLoaded; }
 };
